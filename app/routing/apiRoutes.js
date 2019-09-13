5a30bd9ad6f029e92ed22b9aeb147a8fe1b4dba3
connor-durham <connordurham@connors-mbp.home>
2019-09-12 22:21:13 -0400

    committing initial app

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+
+const friendsList = require("../data/friends");
+
+module.exports = function(app) {
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    let newFriendScores = req.body;
+    console.log(newFriendScores)
+
+    friendsList.push(req.body);
+
+  });
+
+
+  app.post("/api/clear", function(req, res) {
+    friends.length = 0;
+    res.json({ ok: true });
+  });
+};
